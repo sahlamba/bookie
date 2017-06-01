30ad0037ace675583ca205e4a40af6da839913eb
Sahil Lamba <sahil.lamba95@gmail.com>
2017-06-01 17:52:35 +0530

    Add Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React from 'react';
+import {
+  View,
+  ActivityIndicator,
+  StyleSheet
+} from 'react-native';
+
+const Loader = () => (
+  <View style={styles.loader}>
+		<ActivityIndicator size="large" color="#303841" />
+	</View>
+);
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default Loader;
